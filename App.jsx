8b609c64f6b9b5f4525a5dafa3685fc1e5190e4c
Sanjan Prabu <Sanjuprabu2010@gmail.com>
2025-07-31 17:32:53 +0530

    First git commit to clear the uncommited changes-- stable bersion.

---
+++ App.jsx
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
+import { Text, StyleSheet, View } from 'react-native';
+
+function App() {
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        ad;kasd;fkasd
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+  },
+  text:{
+    color:"red",
+
+  }
+});
+
+export default App;
